6d6cd1772bd1a01bedd0e3898725bc8989184302
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-21 14:07:09 +0530

    variables in golang

---
+++ 3_variables/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "golang"
+	var name2 = "goland"
+	fmt.Println(name)
+	fmt.Println(name2)
+
+	var isAge = true
+	fmt.Println(isAge)
+
+	var age int = 10
+	fmt.Println(age)
+
+	var number float32 = 30.6
+	fmt.Println(number)
+
+	// short hand syntax
+	language := "golang"
+	fmt.Println(language)
+
+	price := 350.5
+
+	fmt.Println(price)
+}
