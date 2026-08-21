1875709e069ab3298ac4dfabf534320a34133c26
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-21 13:58:23 +0530

    print hello world and simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 1) // integer
+
+	fmt.Println("Hello Golang") // string
+
+	fmt.Println(true) // bool
+
+	fmt.Println(10.45) // floats
+
+	fmt.Println(7.34 / 3.6)
+}
