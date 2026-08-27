e654e3b424e758e8618e3f2fa06eb20968e170f7
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-27 17:16:46 +0530

    interface

---
+++ 17_interface/interface.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type paymenter interface {
+	pay(amount float32)
+}
+
+type payment struct{
+	gateway paymenter
+}
+
+func (p payment) makePayment(amount float32) {
+	// razorpayPaymentGw := razorpay{}
+	// razorpayPaymentGw.pay(amount)
+	p.gateway.pay(amount)
+}
+
+type razorpay struct{}
+
+func (r razorpay) pay(amount float32) {
+	fmt.Println("Making payment using razorpay", amount)
+}
+
+type stripe struct{}
+
+func (s stripe) pay(amount float32) {
+	fmt.Println("Making payment using stripe", amount)
+}
+
+func main() {
+	// stripePaymentGw := stripe{}
+	razorpayPaymentGw := razorpay{}
+	newPayment := payment{
+		gateway: razorpayPaymentGw,
+	}
+	newPayment.makePayment(100)
+}
