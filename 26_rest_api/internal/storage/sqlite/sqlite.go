b374b0c4aa8c5d07f9bbeae6826109ed6be6664d
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-09-03 16:07:21 +0530

    add GET endpoint to retrieve all students and implement corresponding storage method

--- 26_rest_api/internal/storage/sqlite/sqlite.go
+++ 26_rest_api/internal/storage/sqlite/sqlite.go
@@ -39,12 +39,12 @@ func (s *Sqlite) CreatStudent(name string, email string, age int) (int64, error)
 	stmt, err := s.Db.Prepare("INSERT INTO students (name, email, age) VALUES (?, ?, ?)")
 
 	if err != nil {
-		return  0, nil
+		return 0, nil
 	}
 
 	defer stmt.Close()
 
-	result , err := stmt.Exec(name, email, age)
+	result, err := stmt.Exec(name, email, age)
 
 	if err != nil {
 		return 0, err
@@ -59,7 +59,7 @@ func (s *Sqlite) CreatStudent(name string, email string, age int) (int64, error)
 	return lastId, nil
 }
 
-func (s *Sqlite) GetStudentById(id int64) (types.Student, error ){
+func (s *Sqlite) GetStudentById(id int64) (types.Student, error) {
 	stmt, err := s.Db.Prepare(`SELECT id, name, email, age FROM students WHERE id=? LIMIT 1`)
 
 	if err != nil {
@@ -73,11 +73,47 @@ func (s *Sqlite) GetStudentById(id int64) (types.Student, error ){
 	err = stmt.QueryRow(id).Scan(&student.Id, &student.Name, &student.Email, &student.Age)
 
 	if err != nil {
-		if err == sql.ErrNoRows{
-			return types.Student{}, fmt.Errorf("no student found with id: %s",fmt.Sprint(id))
+		if err == sql.ErrNoRows {
+			return types.Student{}, fmt.Errorf("no student found with id: %s", fmt.Sprint(id))
 		}
-		return types.Student{}, fmt.Errorf("query error: %w",err)
+		return types.Student{}, fmt.Errorf("query error: %w", err)
 	}
 
 	return student, nil
+}
+
+func (s *Sqlite) GetStudents() ([]types.Student, error) {
+	stmt, err := s.Db.Prepare(`SELECT id, name, email, age FROM students`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var students []types.Student
+
+	for rows.Next() {
+		var student types.Student
+		err = rows.Scan(
+			&student.Id,
+			&student.Name,
+			&student.Email,
+			&student.Age,
+		)
+		if err != nil {
+			return nil, err
+		}
+		students = append(students, student)
+	}
+
+	return students, nil
 }
--- 26_rest_api/internal/storage/storage.go
+++ 26_rest_api/internal/storage/storage.go
@@ -1,7 +1,9 @@
 package storage
 
 import "github.com/itz-prashant/student-api/internal/types"
+
 type Storage interface {
 	CreatStudent(name string, email string, age int) (int64, error)
 	GetStudentById(id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
