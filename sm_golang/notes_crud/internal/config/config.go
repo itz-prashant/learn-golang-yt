043fc8cc3166a7e12077a90fc1bb55970d0a2cd6
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-09-10 16:37:10 +0530

    add configuration management with dotenv support and environment variable extraction

--- sm_golang/notes_crud/internal/config/config.go
+++ sm_golang/notes_crud/internal/config/config.go
@@ -1,1 +1,55 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	MongoUri   string
+	MongoDb    string
+	ServerPort string
+}
+
+func extractEnv(key string) (string, error) {
+	val := os.Getenv(key)
+
+	if val == "" {
+		return "", fmt.Errorf("Missing req env")
+	}
+
+	return val, nil
+}
+
+func Load() (Config, error) {
+
+	if err := godotenv.Load(); err != nil {
+		return Config{}, fmt.Errorf("Failed to load env")
+	}
+
+	mongoUri, err := extractEnv("MONGO_URI")
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	mongoDbName, err := extractEnv("MONGO_DB_NAME")
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	port, err := extractEnv("PORT")
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	return Config{
+		MongoUri:   mongoUri,
+		MongoDb:    mongoDbName,
+		ServerPort: port,
+	}, nil
+}
